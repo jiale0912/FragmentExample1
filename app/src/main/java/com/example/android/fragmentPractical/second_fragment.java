6033e0be1fc1ef869ec40a552cbe31c9f4433699
jllim-wt15@student.tarc.edu.my <Jiale1997>
2019-07-01 14:26:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/fragmentPractical/second_fragment.java
@@ -0,0 +1,41 @@
+package com.example.android.fragmentPractical;
+
+
+import android.media.Rating;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class second_fragment extends Fragment {
+
+
+    public second_fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView =  inflater.inflate(R.layout.fragment_second_fragment, container, false);
+        final RatingBar ratingBar = rootView.findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(getContext(),"Rating is " + rating ,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+                return rootView;
+    }
+
+}
